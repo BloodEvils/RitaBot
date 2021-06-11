accfbe5f37725c1995b381666a164b51262cae80
BloodEvils <79896883+BloodEvils@users.noreply.github.com>
2021-06-11 15:52:52 +0300

    Update events.js

--- src/events.js
+++ src/events.js
@@ -94,7 +94,7 @@ exports.listen = function listen (client)
 
          client.user.setPresence({
             "activity": {
-               "name": "ritabot.gg | !tr help",
+               "name": "ĐȺnǥɇɍøᵾs | ɃłøøđɆvɨłs",
                "type": "PLAYING"
             },
             "status": "online"
